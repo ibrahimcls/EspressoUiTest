fc68774edc6f59b5b4d318d5ce672dec1f13fb13
ibrahim <ibrahimcals.11@gmail.com>
2022-04-26 00:18:32 +0300

    intent gallery test

--- app/src/androidTest/java/com/ic/uitest/MainActivityTest.kt
+++ app/src/androidTest/java/com/ic/uitest/MainActivityTest.kt
@@ -1,35 +1,60 @@
 package com.ic.uitest
 
-import androidx.test.core.app.ActivityScenario
+import android.app.Activity.RESULT_OK
+import android.app.Instrumentation
+import android.content.ContentResolver
+import android.content.Intent
+import android.content.res.Resources
+import android.net.Uri
+import android.provider.MediaStore
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasData
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import junit.framework.TestCase
+import androidx.test.platform.app.InstrumentationRegistry
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
-class MainActivityTest : TestCase() {
+class MainActivityTest{
 
+    @get:Rule
+    val intentsTestRule = IntentsTestRule(MainActivity::class.java)
 
     @Test
-    fun test_navSecondaryActivity() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-        onView(withId(R.id.button_next_activity)).perform(click())
-        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+    fun  test_validateIntentSentToPickPackage() {
+
+        // GIVEN
+        val expectedIntent: Matcher<Intent> = allOf(
+            hasAction(Intent.ACTION_PICK),
+            hasData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        )
+        val activityResult = createGalleryPickActivityResultStub()
+        intending(expectedIntent).respondWith(activityResult)
+
+        // Execute and Verify
+        onView(withId(R.id.button_open_gallery)).perform(click())
+        intended(expectedIntent)
     }
 
-    @Test
-    fun test_backPressToMainActivity() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-        onView(withId(R.id.button_next_activity)).perform(click())
-        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
-        //onView(withId(R.id.button_back)).perform(click())
-        pressBack()
-        onView(withId(R.id.main)).check(matches(isDisplayed()))
-
+    private fun createGalleryPickActivityResultStub(): Instrumentation.ActivityResult {
+        val resources: Resources = InstrumentationRegistry.getInstrumentation().context.resources
+        val imageUri = Uri.parse(
+            ContentResolver.SCHEME_ANDROID_RESOURCE + "://" +
+                    resources.getResourcePackageName(R.drawable.ic_launcher_background) + '/' +
+                    resources.getResourceTypeName(R.drawable.ic_launcher_background) + '/' +
+                    resources.getResourceEntryName(R.drawable.ic_launcher_background)
+        )
+        val resultIntent = Intent()
+        resultIntent.setData(imageUri)
+        return Instrumentation.ActivityResult(RESULT_OK, resultIntent)
     }
 }
